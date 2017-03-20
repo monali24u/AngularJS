4adcebd5f29ce9db3b17ef84ba52ce68a74eae6d
Monali Patil <monali24u@gmail.com>
2017-03-19 20:29:53 -0600

    added click eventHandler which on click make ajax request and get the list of all contries and shows them on page.

--- ajax-countries/controller/mainController.js
+++ ajax-countries/controller/mainController.js
@@ -3,10 +3,26 @@ var app = angular.module("myApp", []);
 
 app.controller('viewCntl', function($scope, $http){
 
+  //ajax request
   $http.get("/countries.json")
   .then(function(response) {
     console.log("come here!!");
       $scope.countiresData = response.data;
   });
+
+});
+
+app.controller('secondCntl', function($scope){
+
+  $scope.clicked = false;
+
+  $scope.showList = function(){
+    if($scope.clicked == false){
+      $scope.clicked = true;
+    }
+    else{
+      $scope.clicked = false;
+    }
+  }
 
 });
